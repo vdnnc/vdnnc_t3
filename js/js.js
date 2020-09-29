3716fa1b61fbc5dee5b41acc0c32758559511844
T.Ang <loveos@foxmail.com>
2020-09-29 22:23:44 +0800

    布局响应优化

---
+++ js/js.js
@@ -0,0 +1,4 @@
+function vdnnc() {
+document.writeln("<link href=\'https://e.vdnnc.com/tcdn/vdnnct3/css/bootstrap.css\' rel=\'stylesheet\'>");
+document.writeln("UI provided <a href=\'https://gitee.com/vdnnc/vdnnc_t3.git\' target=\'_blank\'>VDnncT3</a>");
+}
